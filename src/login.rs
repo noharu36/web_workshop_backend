826a12d85e920a237b2aef08d42142581c4e4fa8
noharu36 <132886617+noharu36@users.noreply.github.com>
2023-11-08 19:49:15 +0900

    ログイン認証の実装

---
+++ src/login.rs
@@ -0,0 +1,55 @@
+use mongodb::{Collection, Client, options::ClientOptions, bson::doc};
+use futures::stream::TryStreamExt;
+use serde::{Deserialize, Serialize};
+use axum::Json;
+
+
+//Json変換
+#[derive(Serialize)]
+pub struct Authentication {
+    pub name: String,
+    pub password: String,
+    pub auth: bool
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    pub name: String,
+    pub password: String,
+}
+
+pub async fn connect_db() -> Collection::<User> {
+    let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+    let client = Client::with_options(client_options).unwrap();
+
+
+    client.database("test").collection::<User>("user")
+}
+
+pub async fn check_user(n: String, p: String) -> Option<User> {
+    let coll = connect_db().await;
+    let filter = doc!{"name": n, "password": p};
+    let mut cursor = coll.find(filter, None).await.unwrap();
+
+    while let Some(doc) = cursor.try_next().await.unwrap() {
+        return Some(doc);
+    }
+
+    None
+
+}
+
+pub async fn auth(Json(element): Json<User>) -> Json<Authentication> {
+    if let Some(usr) = check_user(element.name, element.password).await {
+        Json(Authentication {
+            name: usr.name,
+            password: usr.password,
+            auth: true })
+    } else {
+        Json(Authentication {
+            name: String::from("err"),
+            password: String::from("err"),
+            auth: false
+        })
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use axum::{
+    routing::post,
+    http::{StatusCode, header::HeaderName, HeaderValue, Request},
+    //Json,
+    Router,
+    middleware::Next,
+    response::Response,
+};
+use tower::ServiceBuilder;
+use std::net::SocketAddr;
+use tower_http::{request_id::{MakeRequestId, PropagateRequestIdLayer, RequestId, SetRequestIdLayer,},cors::{Any, CorsLayer}};
+use tracing::info;
+use uuid::Uuid;
+use sns_backend::login::auth;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let x_request_id = HeaderName::from_static("x-request-id");
+
+    let app = Router::new()
+        .route("/login", post(auth))
+        //.route("/get", get(root))
+        .layer(
+            CorsLayer::new()
+            .allow_origin(Any)
+            .allow_headers(Any)
+            .allow_methods(Any)
+        )
+        .layer(
+            ServiceBuilder::new()
+                .layer(SetRequestIdLayer::new(
+                    x_request_id.clone(),
+                    MyRequestId::new(),
+                ))
+                .layer(PropagateRequestIdLayer::new(x_request_id))
+                .layer(axum::middleware::from_fn(
+                    access_log_on_request,
+                )),
+        );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+
+//アクセスログの出力
+#[derive(Clone)]
+pub struct MyRequestId {}
+
+impl MyRequestId {
+    pub fn new() -> Self {
+        MyRequestId {}
+    }
+}
+
+impl MakeRequestId for MyRequestId {
+    fn make_request_id<B>(&mut self, _request: &Request<B>) -> Option<RequestId> {
+        let uuid = Uuid::new_v4().to_string();
+        let request_id = HeaderValue::from_str(&uuid).unwrap();
+
+        Some(RequestId::new(request_id))
+    }
+}
+
+async fn access_log_on_request<B>(req: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
+    info!("{} {}", req.method(), req.uri());
+    Ok(next.run(req).await)
+}
+
+
+
+//curlのコマンド curl localhost:8080/login -XPOST -H 'Content-Type: application/json' -d '{"name": "haru", "password": "pass"}'
