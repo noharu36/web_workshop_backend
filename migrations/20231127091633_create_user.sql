edd58ea8e0b6cc3f5e462c57604e1c7acd517ca4
noharu36 <132886617+noharu36@users.noreply.github.com>
2023-12-03 18:19:10 +0900

    ルームの作成ができるように

---
+++ migrations/20231127091633_create_user.sql
@@ -0,0 +1,5 @@
+-- Add migration script here
+INSERT INTO users(name, password) VALUES ('haru', 'pass');
+INSERT INTO users(name, password) VALUES ('test1', 'abcd');
+INSERT INTO users(name, password) VALUES ('test2', 'efgh');
+INSERT INTO users(name, password) VALUES ('user', 'prog');
--- src/make_thread.rs
+++ src/make_thread.rs
@@ -1,31 +1,34 @@
-use mongodb::bson::doc;
-use serde::{Deserialize, Serialize};
-use axum::Json;
 use crate::connect_database;
+use axum::Json;
 use chrono::Local;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct ReceivedInfo {
     pub title: String,
-    pub body: String,
-    pub user_id: String,
+    pub subject: String,
+    pub place: String,
+    pub purpose: String,
+    pub comment: String,
+    pub user_id: i32,
     pub user_name: String,
 }
 
 #[derive(Serialize)]
 pub struct Thread {
     pub title: String,
-    pub body: String,
+    pub subject: String,
+    pub place: String,
+    pub purpose: String,
+    pub comment: String,
     pub url: String,
-    pub user_id: String,
+    pub user_id: i32,
     pub user_name: String,
-    pub create_at: String
+    pub create_at: String,
 }
 
 pub async fn create_thread(Json(element): Json<ReceivedInfo>) {
-    let db = connect_database::connect_db().await;
-    let coll = db.collection::<Thread>("threads");
-
+    let db = connect_database::connect_db().await.unwrap();
     let mut url = String::from("thread");
     let cnt = (count_thread().await + 1).to_string();
     url += &cnt;
@@ -33,21 +36,30 @@ pub async fn create_thread(Json(element): Json<ReceivedInfo>) {
     let dt = Local::now();
     let time = dt.format("%Y-%m-%d %H:%M").to_string();
 
-    let thread = Thread {
-        title: element.title,
-        body: element.body,
-        url: url,
-        user_id: element.user_id,
-        user_name: element.user_name,
-        create_at: time
-    };
-
-    coll.insert_one(thread, None).await.unwrap();
+    sqlx::query!(
+        r#"
+            INSERT INTO threads(title, subject, place, purpose, comment, url, user_id, user_name, create_at)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+        "#,
+        element.title,
+        element.subject,
+        element.place,
+        element.purpose,
+        element.comment,
+        url,
+        element.user_id,
+        element.user_name,
+        time
+    ).execute(&db).await.ok();
 }
 
-pub async fn count_thread() -> u64 {
-    let db = connect_database::connect_db().await;
-    let coll = db.collection::<Thread>("threads");
-
-    coll.count_documents(None, None).await.ok().unwrap()
+pub async fn count_thread() -> i64 {
+    let db = connect_database::connect_db().await.ok().unwrap();
+    sqlx::query!("SELECT COUNT(*) as count FROM threads")
+        .fetch_one(&db)
+        .await
+        .unwrap()
+        .count
 }
+
+//curlのコマンド curl localhost:8080/create -XPOST -H 'Content-Type: application/json' -d '{"title": "test-thread", "subject": "数学", "place": "UBIC", "purpose": "期末対策", "comment": "てすと", "user_id": 1, "user_name": "haru"}'
